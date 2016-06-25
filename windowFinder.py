b4921f4ff359983869d58c5b6391c3d276ca2f25
fedhere <fedhere@gmail.com>
2016-06-25 10:05:33 -0400

    commeinting winodw finder code

--- windowFinder.py
+++ windowFinder.py
@@ -2,6 +2,11 @@ from __future__ import print_function
 ##CUSP UO 2016
 __author__ = "fbb"
 
+'''    
+Fetches windows )light sources) from an image stack of a dense urban landscape.
+'''
+
+
 import numpy as np
 import scipy as sp
 import pylab as pl
@@ -10,8 +15,18 @@ import matplotlib.gridspec as gridspec
 from scipy import ndimage
 from scipy import optimize
 
-def getknuth(m,data,N):
+def getknuth(m,data,N=None):
+    '''Calculates the likelihood of the proposed bin size according to Knuth's rule
+
+    Arguments:
+        the proposed bin size, data array (1D numpy array), data size
+    Returns: 
+        Knuth's likelihood
+    '''
     m=int(m)
+    if N is None:
+        N = data.size
+
     if m > N:
         return -1
     bins=np.linspace(min(data),max(data), int(m) + 1)
@@ -23,6 +38,17 @@ def getknuth(m,data,N):
 
     
 def knuthn(data):
+    '''
+    Finds the right bin size according to Knuth's rule (Bayesian inference)
+
+    Arguments: 
+        data array. must be a 1D numpy array
+    Returns:
+        optimized value for bin size according to Knuth's rule.
+    Calls: 
+        getknuth()
+        scipy.optimize.fmin()
+    ''' 
     assert data.ndim==1, "data must be 1D array to calculate Knuth's number of bins"
     N = data.size
     maxM = 5 * np.sqrt(N)
@@ -38,22 +64,61 @@ def knuthn(data):
     return mk
 
 GS = 10 #gaussian standard deviation (x and y)
+
 if __name__=='__main__':
+    """
+    Fetches windows from an image stack.
+
+    High-pass filters the image and thresholds the residuals to selects windowslight cources.
+
+    Args:
+        imge in in put. should be a stack of images, with reasonable SNR, 
+        as a npy array. 
+
+        Optional Args:
+        thr: the threshold guess to be verified
+        noauto: if True the first attempted guess is thr=110, 
+                iif False instead  the first attempted guess is 
+                thr=90th percent limit of the pixel distribution
+        nocheck: does not plot the image and just goes for 90th percentile.
+                 otherwise you re asked to confirm the threshold
+        showme: if True will show the labelled and selected window 
+                images before saving
+    Returns:
+        Nothing: saves a npy image with pixels 0'ed away from windows. 
+           Saves: 
+                all sources above threshold and the histogram in a PDF image
+                (_allwindows.pdf)
+                all good (>10 pixel) labelled windows in a npy array _labels.py 
+                an image of the labelled windows in a npy _labelledwindows.npy
+                an image of the labelled windows in PDF _labelledwindows.pdf
+                an image of the selected windows away from edges PDF _selected.pdf
+                a npy of the coordinates (center of brightness) of all selected windos
+                a npy mask that can be ised witht he orignal image
+    Comments:
+        Its a bit slow, so be patient...
+    """
+
 
     parser = optparse.OptionParser(usage="windowFinder 'stackpath' ",
                                    conflict_handler="resolve")
 
     parser.add_option('--thr', default=110, type="float",
                       help='threshold for window')
+    
     parser.add_option('--noauto', default=True, action="store_false",
-                      help='automaticallyl setting the threshold to 98% of pixel value distribution')
+                      help='''automaticallyl setting the threshold 
+                      to 98% of pixel value distribution''')
+    
     parser.add_option('--nocheck', default=False, action="store_true",
-                      help='dont check if threshold is right')        
+                      help='dont check if threshold is right')
+    
     parser.add_option('--showme', default=False, action="store_true",
                       help='show plots')
 
     options,  args = parser.parse_args()
-    print (options)
+    if len(args)<1:
+        print (options)
     '''
     thr=200
     args = [0]
@@ -80,7 +145,6 @@ if __name__=='__main__':
     widths = np.diff(bins)
     countsnorm = counts/counts.max()
     #show the stacks
-    #done = True
     done = options.nocheck
     if not done:
         pl.ion()
@@ -90,11 +154,11 @@ if __name__=='__main__':
         ax0 = stackfig.add_subplot(gs[0, 0])
         ax0.imshow(foo.clip(thr/3,255).astype(np.uint8), interpolation='nearest')
         ax0.set_title("gaussian filter residuals, gaussian size: %d"%GS,
-                      fontsize=13)
+                      fontsize=10)
         ax0.axis('off')
         ax1 = stackfig.add_subplot(gs[0, 1])
         ax1.set_title("thresholded image, threshold: %.2f"%thr,
-                      fontsize=13)
+                      fontsize=10)
         ax1.imshow(foo.mean(-1)>thr, interpolation='nearest', cmap='gist_gray')
         ax1.axis('off')
         ax3 = stackfig.add_subplot(gs[1, :])
@@ -114,7 +178,7 @@ if __name__=='__main__':
                  ha='right', color='red')
             
         
-        ax3.set_xlim(pc[0], max(pc[-1], thr))
+        ax3.set_xlim(pc[0], max(pc[-2], thr))
         #pl.show()    
     while not done:
         pl.draw()
